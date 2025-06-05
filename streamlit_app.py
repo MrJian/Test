f1044c15eb5cf5a18da25472ee041224518a76a4
MrJian <1013984172@qq.com>
2025-06-05 20:19:10 +0800

    Improve Streamlit app and fix push URL newline

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
-import streamlit as st
- 
-st.write("""
-# My first app
-Hello *world!*
-""")
+"""A minimal Streamlit app."""
+
+try:
+    import streamlit as st
+except ModuleNotFoundError as exc:
+    raise SystemExit(
+        "Streamlit is required to run this app."
+    ) from exc
+
+
+st.write(
+    """# My first app
+Hello *world!*"""
+)
